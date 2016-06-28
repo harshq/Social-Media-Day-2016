713291c07d49b29cf04876b281fef6b59eafaf9e
Harshana Abeyaratne <HarshanaA@99x.lk>
2016-06-28 16:46:30 +0530

    added enpoint for tweet count

---
+++ js/smday.js
@@ -0,0 +1,45 @@
+  (function($){
+     
+     "use strict"; 
+
+   $(function(){
+        var options = {
+          useEasing : true, 
+          useGrouping : true, 
+          separator : ',', 
+          decimal : '.', 
+          prefix : '', 
+          suffix : '' 
+        };
+        
+        var twitterCount = 0;
+        
+        $.get('https://3uglvu7ksf.execute-api.us-east-1.amazonaws.com/dev/tweetercount', function(data){
+            twitterCount = data.count;
+            var demo = new CountUp("animate-count", 0, twitterCount, 0, 2.5, options);
+            demo.start();  
+        });
+        
+        setTimeout(function(){
+            $('.loader-bg').fadeOut("slow");
+            
+            setTimeout(function () {
+                $('body').removeClass('no-scroll');   
+            }, 500);
+             
+        }, 500);
+        
+       
+        $(window).scroll(function(e){
+             if($(this).scrollTop()>300){
+                    $(".twitter-textbox-text").typed({
+                        strings: ["I like to Appreciate", "<b>#IAppreciate</b> Harindu's awesome work^500 with flood relief through Red Cross^600 - nominating <b>@Achini^600, @Prasanna^250, @Charana^500</b> to keep appreciating!^600 <b>:)</b>"],
+                        typeSpeed: 0.4,
+                        contentType: 'html'
+                    });
+             }
+        });
+ 
+    });
+  
+  })(jQuery);
