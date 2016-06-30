baa73868b6b2d479ecbad0f609adda3604315166
Harshana Abeyaratne <harshanaa@99x.lk>
2016-06-30 11:39:01 +0530

    minor bug fixes

--- js/smday.js
+++ js/smday.js
@@ -17,17 +17,26 @@
         $.get('https://3uglvu7ksf.execute-api.us-east-1.amazonaws.com/dev/tweetercount', function(data){
             twitterCount = data.count;
             var demo = new CountUp("animate-count", 0, twitterCount, 0, 2.5, options);
-            demo.start();  
+            
+            
+            
+            $('<img/>').attr('src', 'http://iappreciate.lk/img/bg_image.jpg').load(function() {
+            $(this).remove();
+            $('.bg-main').css('background-image', 'url(http://iappreciate.lk/img/bg_image.jpg)');
+            $('.loader-bg').fadeOut("slow");
+                setTimeout(function () {
+                    demo.start();  
+                    $('body').removeClass('no-scroll');   
+                }, 500);  
+            });
+            
+            
+            
+            
+            
         });
         
-        $('<img/>').attr('src', 'http://iappreciate.lk/img/bg_image.jpg').load(function() {
-           $(this).remove();
-           $('.bg-main').css('background-image', 'url(http://iappreciate.lk/img/bg_image.jpg)');
-           $('.loader-bg').fadeOut("slow");
-            setTimeout(function () {
-                $('body').removeClass('no-scroll');   
-            }, 500); 
-        });
+        
 
         $(window).scroll(function(e){
              if($(this).scrollTop()>300){
